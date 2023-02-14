b02c01e88c17590233a5a0037880981d2c6cef7e
lmarquardt7 <lmarquardt7@student.cvtc.edu>
2023-02-14 01:46:26 -0600

    Implemented newFolderName function in server.js

--- server.js
+++ server.js
@@ -1,3 +1,32 @@
 const newFolderName = function (folders) {
 
+  if (folders.includes('New Folder')){
+
+    let i = 2
+    while(i <= (folders.length + 1)){
+      if (folders.includes( 'New Folder (' + i + ')')){
+        i++
+      } else {
+        folders.push('New Folder (' + (i) + ')')
+        break
+      }
+    }
+  } else {
+    folders.push('New Folder')
+  }
+
+  return folders;
+
 }
+
+
+
+
+console.log(newFolderName([]))
+console.log(newFolderName(['test']))
+console.log(newFolderName(['New Folder (2)']))
+
+console.log(newFolderName(['New Folder']))
+console.log(newFolderName(['New Folder', 'New Folder (2)']))
+console.log(newFolderName(['New Folder', 'New Folder (2)', 'New Folder (3)']))
+console.log(newFolderName(['New Folder', 'New Folder (2)', 'New Folder (3)', 'New Folder (4)']))
